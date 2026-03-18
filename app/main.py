6e1291f5570eb510009cf89832354de6b735e1db
Daniil Yakovlev <313174313@mail.ru>
2026-03-18 23:13:53 +0300

    add pydantic schemas for tasks

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class TaskCreate(BaseModel):
+    title: str
+    completed: bool
+
+class TaskResponse(BaseModel):
+    title: str
+    completed: bool
+
+
+tasks = []
 
 
 @app.get("/")
 def read_root():
     return {"message": "First API"}
+
+@app.get("/tasks", response_model=list[TaskResponse])
+def get_tasks():
+    return tasks
+
+@app.post("/tasks")
+def add_task(task: TaskCreate):
+    tasks.append(task.model_dump())
+    return {"message": "Task created"}
 
